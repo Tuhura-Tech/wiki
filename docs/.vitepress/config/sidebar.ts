194901d5abcf2560bee7095cdc6b6b817d183538
Boop <onpotis@gmail.com>
2023-04-05 07:28:54 +1200

    Update sidebar.ts

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -1,77 +1,80 @@
 export default {
-  "/ncea/level-1": [
-    {
-      text: "NCEA Level 1",
-      link: "/ncea/level-1",
-      items: [],
-    },
-  ],
-  "/ncea/level-2": [
-    {
-      text: "NCEA Level 2",
-      link: "/ncea/level-2",
-      items: [
+    "/ncea/level-1": [
         {
-          text: "Web Internal",
-          link: "/ncea/level-2/web-flask",
+            text: "NCEA Level 1",
+            items: [],
         },
-      ],
-    },
-  ],
-  "/ncea/level-3": [
-    {
-      text: "NCEA Level 3",
-      link: "/ncea/level-3",
-      items: [],
-    },
-  ],
-  "/guides/3d-printing": [
-    {
-      text: "3D Printing",
-      link: "/guides/3d-printing",
-      items: [],
-    },
-  ],
-  "/guides/cybersecurity": [
-    {
-      text: "Cyber Security",
-      link: "/guides/cybersecurity",
-      items: [],
-    },
-  ],
-  "/guides/electronics": [
-    {
-      text: "Electronics",
-      link: "/guides/electronics",
-      items: [],
-    },
-  ],
-  "/guides/python": [
-    {
-      text: "Python",
-      link: "/guides/python",
-      items: [{ text: "Setting up", link: "/guides/python/setting-up" }],
-    },
-  ],
-  "/guides/rust": [
-    {
-      text: "Rust",
-      link: "/guides/rust",
-      items: [],
-    },
-  ],
-  "/guides/java": [
-    {
-      text: "Java",
-      link: "/guides/java",
-      items: [],
-    },
-  ],
-  "/guides/git": [
-    {
-      text: "Git",
-      link: "/guides/git",
-      items: [],
-    },
-  ],
+    ],
+    "/ncea/level-2": [
+        {
+            text: "NCEA Level 2",
+            items: [
+                {
+                    text: "Web Internal using Flask",
+                    link: "/ncea/level-2/web-flask",
+                },
+            ],
+        },
+    ],
+    "/ncea/level-3": [
+        {
+            text: "NCEA Level 3",
+            items: [],
+        },
+    ],
+    "/guides/3d-printing": [
+        {
+            text: "3D Printing",
+            items: [],
+        },
+    ],
+    "/guides/cybersecurity": [
+        {
+            text: "Cyber Security",
+            items: [],
+        },
+    ],
+    "/guides/electronics": [
+        {
+            text: "Electronics",
+            items: [],
+        },
+    ],
+    "/guides/python": [
+        {
+            text: "Python",
+            items: [{ text: "Setting up", link: "/guides/python/setting-up" }],
+        },
+    ],
+    "/guides/rust": [
+        {
+            text: "Rust",
+            items: [],
+        },
+    ],
+    "/guides/java": [
+        {
+            text: "Java",
+            items: [],
+        },
+    ],
+    "/guides/git": [
+        {
+            text: "Git",
+            items: [
+                {
+                    text: "Setting Up",
+                    link: "/guides/git/setting-up",
+                },
+                {
+                    text: "Git Basics",
+                    link: "/guides/git/basics",
+                },
+                {
+                    text: "Tips and Tricks",
+                    link: "/guides/git/tips-and-tricks",
+                },
+            ],
+        },
+    ],
 };
