6ba3b34bf4d3fc8a31f3cc3990dade59e964ace0
Boop <onpotis@gmail.com>
2023-04-05 07:31:34 +1200

    I literally replaced it with the same exact file from the main repo because of the merge issues

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -1,80 +1,80 @@
 export default {
-    "/ncea/level-1": [
+  "/ncea/level-1": [
+    {
+      text: "NCEA Level 1",
+      items: [],
+    },
+  ],
+  "/ncea/level-2": [
+    {
+      text: "NCEA Level 2",
+      items: [
         {
-            text: "NCEA Level 1",
-            items: [],
+          text: "Web Internal using Flask",
+          link: "/ncea/level-2/web",
         },
-    ],
-    "/ncea/level-2": [
+      ],
+    },
+  ],
+  "/ncea/level-3": [
+    {
+      text: "NCEA Level 3",
+      items: [],
+    },
+  ],
+  "/guides/3d-printing": [
+    {
+      text: "3D Printing",
+      items: [],
+    },
+  ],
+  "/guides/cybersecurity": [
+    {
+      text: "Cyber Security",
+      items: [],
+    },
+  ],
+  "/guides/electronics": [
+    {
+      text: "Electronics",
+      items: [],
+    },
+  ],
+  "/guides/python": [
+    {
+      text: "Python",
+      items: [{ text: "Setting up", link: "/guides/python/setting-up" }],
+    },
+  ],
+  "/guides/rust": [
+    {
+      text: "Rust",
+      items: [],
+    },
+  ],
+  "/guides/java": [
+    {
+      text: "Java",
+      items: [],
+    },
+  ],
+  "/guides/git": [
+    {
+      text: "Git",
+      items: [
         {
-            text: "NCEA Level 2",
-            items: [
-                {
-                    text: "Web Internal using Flask",
-                    link: "/ncea/level-2/web",
-                },
-            ],
+          text: "Setting Up",
+          link: "/guides/git/setting-up",
         },
-    ],
-    "/ncea/level-3": [
         {
-            text: "NCEA Level 3",
-            items: [],
+          text: "Git Basics",
+          link: "/guides/git/basics",
         },
-    ],
-    "/guides/3d-printing": [
         {
-            text: "3D Printing",
-            items: [],
+          text: "Tips and Tricks",
+          link: "/guides/git/tips-and-tricks",
         },
-    ],
-    "/guides/cybersecurity": [
-        {
-            text: "Cyber Security",
-            items: [],
-        },
-    ],
-    "/guides/electronics": [
-        {
-            text: "Electronics",
-            items: [],
-        },
-    ],
-    "/guides/python": [
-        {
-            text: "Python",
-            items: [{ text: "Setting up", link: "/guides/python/setting-up" }],
-        },
-    ],
-    "/guides/rust": [
-        {
-            text: "Rust",
-            items: [],
-        },
-    ],
-    "/guides/java": [
-        {
-            text: "Java",
-            items: [],
-        },
-    ],
-    "/guides/git": [
-        {
-            text: "Git",
-            items: [
-                {
-                    text: "Setting Up",
-                    link: "/guides/git/setting-up",
-                },
-                {
-                    text: "Git Basics",
-                    link: "/guides/git/basics",
-                },
-                {
-                    text: "Tips and Tricks",
-                    link: "/guides/git/tips-and-tricks",
-                },
-            ],
-        },
-    ],
+      ],
+    },
+  ],
 };
