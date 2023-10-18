511508b966f47590107f86435ac4044547a83551
CyberFlame <cyberflameu@gmail.com>
2023-10-18 18:16:46 +1300

    fix(site): add MDX integration so MDX pages will load (#86)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,95 +1,119 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
-
+import mdx from "@astrojs/mdx";
 export const locales = {
-	root: {
-		label: 'English',
-		lang: 'en', // lang is required for root locales
-	},
-	mi: {
-		label: 'Māori',
-		lang: 'mi',
-	},
-};
+  root: {
+    label: 'English',
+    lang: 'en' // lang is required for root locales
+  },
 
+  mi: {
+    label: 'Māori',
+    lang: 'mi'
+  }
+};
 const site = 'https://wiki.tuhuratech.org.nz/';
+
 
 // https://astro.build/config
 export default defineConfig({
-	site,
-
-	integrations: [
-		starlight({
-			title: 'Wiki',
-			logo: {
-				light: './src/assets/logo-light.png',
-				dark: './src/assets/logo-dark.png',
-				replacesTitle: true,
-			},
-			social: {
-				github: 'https://github.com/Tuhura-Tech/Wiki',
-				discord: 'https://discord.gg/PNxh7cwKfQ',
-			},
-			editLink: {
-				baseUrl: 'https://github.com/Tuhura-Tech/Wiki/blob/main/',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: "About our Guides", link: 'guides/about' },
-						{ label: "Javascript", autogenerate: { directory: 'guides/javascript' } },
-						// { label: "Python", autogenerate: { directory: 'guides/python' } },
-						{ label: "Git", autogenerate: { directory: 'guides/git' } },
-						{ label: "Cybersecurity", autogenerate: { directory: 'guides/cybersecurity' } },
-						{ 
-							label: "Game Design", 
-							items: [
-								{ label: "About", link: 'guides/game-design/about' },
-								{ label: "Godot", autogenerate: { directory: 'guides/game-design/godot' } },
-							]
-						},
-					]
-				},
-				
-				{
-					label: 'NCEA Resources',
-					items: [
-						{ label: "About our Resources", link: 'ncea/about' },
-						{ label: "NCEA Level 2", autogenerate: { directory: 'ncea/level-2' } },
-						{ label: "NCEA Level 3", autogenerate: { directory: 'ncea/level-3' } },
-					]
-				},
-				{
-					label: 'Projects',
-					autogenerate: { directory: 'projects' }
-				},
-				{
-					label: "Tuhura Tech Resources",
-					autogenerate: { directory: 'tuhura-tech' }
-				}
-			],
-			locales,
-			favicon: '/images/favicon.svg',
-			head: [
-				// Add ICO favicon fallback for Safari.
-				{
-					tag: 'link',
-					attrs: {
-						rel: 'icon',
-						href: '/images/favicon.ico',
-						sizes: '32x32',
-					},
-				},
-			],
-
-		}),
-		tailwind({
-			// Disable the default base styles:
-			applyBaseStyles: false,
-		}),
-	],
-	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  site,
+  integrations: [starlight({
+    title: 'Wiki',
+    logo: {
+      light: './src/assets/logo-light.png',
+      dark: './src/assets/logo-dark.png',
+      replacesTitle: true
+    },
+    social: {
+      github: 'https://github.com/Tuhura-Tech/Wiki',
+      discord: 'https://discord.gg/PNxh7cwKfQ'
+    },
+    editLink: {
+      baseUrl: 'https://github.com/Tuhura-Tech/Wiki/blob/main/'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [{
+        label: "About our Guides",
+        link: 'guides/about'
+      }, {
+        label: "Javascript",
+        autogenerate: {
+          directory: 'guides/javascript'
+        }
+      },
+      // { label: "Python", autogenerate: { directory: 'guides/python' } },
+      {
+        label: "Git",
+        autogenerate: {
+          directory: 'guides/git'
+        }
+      }, {
+        label: "Cybersecurity",
+        autogenerate: {
+          directory: 'guides/cybersecurity'
+        }
+      }, {
+        label: "Game Design",
+        items: [{
+          label: "About",
+          link: 'guides/game-design/about'
+        }, {
+          label: "Godot",
+          autogenerate: {
+            directory: 'guides/game-design/godot'
+          }
+        }]
+      }]
+    }, {
+      label: 'NCEA Resources',
+      items: [{
+        label: "About our Resources",
+        link: 'ncea/about'
+      }, {
+        label: "NCEA Level 2",
+        autogenerate: {
+          directory: 'ncea/level-2'
+        }
+      }, {
+        label: "NCEA Level 3",
+        autogenerate: {
+          directory: 'ncea/level-3'
+        }
+      }]
+    }, {
+      label: 'Projects',
+      autogenerate: {
+        directory: 'projects'
+      }
+    }, {
+      label: "Tuhura Tech Resources",
+      autogenerate: {
+        directory: 'tuhura-tech'
+      }
+    }],
+    locales,
+    favicon: '/images/favicon.svg',
+    head: [
+    // Add ICO favicon fallback for Safari.
+    {
+      tag: 'link',
+      attrs: {
+        rel: 'icon',
+        href: '/images/favicon.ico',
+        sizes: '32x32'
+      }
+    }]
+  }), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false
+  }), mdx()],
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
