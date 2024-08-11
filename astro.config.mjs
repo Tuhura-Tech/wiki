80fe4733e92e49d7bdccca3ccd3463a328f88f86
Abigail Clennell <abigailaclennell@gmail.com>
2024-08-11 23:09:32 +1200

    fix: added page to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 import starlight from '@astrojs/starlight';
 import { defineConfig } from 'astro/config';
-import starlightLinksValidator from 'starlight-links-validator';
+import starlightLinksValidator from 'starlight-links-validator'
 import remarkMath from 'remark-math';
 import rehypeMathjax from 'rehype-mathjax';
-import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     server: {
       watch: {
-        usePolling: true
-      }
-    }
+        usePolling: true,
+      },
+    },
   },
   site: 'https://wiki.tuhuratech.org.nz/',
   integrations: [starlight({
@@ -33,93 +31,100 @@ export default defineConfig({
       github: 'https://github.com/Tuhura-Tech/Wiki'
     },
     customCss: [
-    // Relative path to your custom CSS file
-    './src/styles/custom.css'],
-    sidebar: [{
-      label: "Python",
-      items: [{
-        label: "Python Basics",
-        link: 'python'
-      }, {
-        label: "Conditionals and Loops",
-        link: "python/conditionals-loops"
-      }, {
-        label: "Lists, Tuples, Dictionaries, and Sets",
-        link: "python/lists-tuples-dicts"
-      }, {
-        label: "Functions and Docstrings",
-        link: "python/functions"
-      }, {
-        label: "Classes and Object-Oriented Programming",
-        link: "python/classes"
-      }, {
-        label: "Turtle",
-        link: "python/turtle"
-      }, {
-        label: "Setting Up",
-        link: 'python/setting-up'
-      }, {
-        label: "Flask",
+      // Relative path to your custom CSS file
+      './src/styles/custom.css',
+    ],
+    sidebar: [
+      {
+        label: "Python", items: [{
+          label: "Python Basics",
+          link: 'python'
+        }, {
+          label: "Conditionals and Loops",
+          link: "python/conditionals-loops"
+        }, {
+          label: "Lists, Tuples, Dictionaries, and Sets",
+          link: "python/lists-tuples-dicts"
+        }, {
+          label: "Functions and Docstrings",
+          link: "python/functions"
+        }, {
+          label: "Classes and Object-Oriented Programming (OOP)",
+          link: "python/classes"
+        }, {
+          label: "Turtle",
+          link: "python/turtle"
+        }, {
+          label: "Setting Up",
+          link: 'python/setting-up'
+        }, {
+          label: "Flask",
+          autogenerate: {
+            directory: 'python/flask'
+          },
+          collapsed: true
+        }]
+      }, 
+      {
+        label: "Game Design",
+        items: [{
+          label: "About",
+          link: 'game-design/index'
+        }, {
+          label: "Godot",
+          autogenerate: {
+            directory: 'game-design/godot'
+          },
+          collapsed: true
+        }],
+      },
+      {
+        label: "Blender",
         autogenerate: {
-          directory: 'python/flask'
-        },
-        collapsed: true
-      }]
-    }, {
-      label: "Game Design",
-      items: [{
-        label: "About",
-        link: 'game-design/index'
-      }, {
-        label: "Godot",
+          directory: 'blender'
+        }
+      },
+      {
+        label: "Cybersecurity",
+
         autogenerate: {
-          directory: 'game-design/godot'
-        },
-        collapsed: true
-      }]
-    }, {
-      label: "Blender",
-      autogenerate: {
-        directory: 'blender'
-      }
-    }, {
-      label: "Cybersecurity",
-      autogenerate: {
-        directory: 'cybersecurity'
-      }
-    }, {
-      label: "SQL",
-      autogenerate: {
-        directory: 'sql'
-      }
-    }, {
-      label: "Javascript",
-      items: [{
-        label: "Setting Up",
-        link: 'javascript/index'
-      }, {
-        label: "Creative Coding",
+          directory: 'cybersecurity'
+        }
+      },
+      {
+        label: "SQL",
         autogenerate: {
-          directory: 'javascript/creative-coding'
-        },
-        collapsed: true
-      }]
-    }, {
-      label: "Git",
-      autogenerate: {
-        directory: 'git'
-      }
-    }, {
-      label: "Tūhura Tech Resources",
-      autogenerate: {
-        directory: 'tuhura-tech'
-      }
-    }],
+          directory: 'sql'
+        }
+      }, {
+        label: "Javascript",
+        items: [{
+          label: "Setting Up",
+          link: 'javascript/index'
+        }, {
+          label: "Creative Coding",
+          autogenerate: {
+            directory: 'javascript/creative-coding'
+          },
+          collapsed: true
+        }]
+      }, {
+        label: "Git",
+        autogenerate: {
+          directory: 'git'
+        }
+      }, {
+        label: "Tūhura Tech Resources",
+        autogenerate: {
+          directory: 'tuhura-tech'
+        }
+      }],
     locales: {
       root: {
         label: 'English',
         lang: 'en' // lang is required for root locales
       },
+
       mi: {
         label: 'Māori',
         lang: 'mi'
@@ -127,19 +132,19 @@ export default defineConfig({
     },
     favicon: '/images/favicon.svg',
     head: [
-    // Add ICO favicon fallback for Safari.
-    {
-      tag: 'link',
-      attrs: {
-        rel: 'icon',
-        href: '/images/favicon.ico',
-        sizes: '32x32'
-      }
-    }],
+      // Add ICO favicon fallback for Safari.
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'icon',
+          href: '/images/favicon.ico',
+          sizes: '32x32'
+        }
+      }],
     plugins: [starlightLinksValidator()],
     components: {
-      Hero: './src/components/starlight/Hero.astro'
-    }
+      Hero: './src/components/starlight/Hero.astro',
+    },
   })],
   markdown: {
     remarkPlugins: [remarkMath],
