fe0695d06de0a85ce9f33f2119a667684cdf4618
Aroha Kiri <arohakiri@gmail.com>
2026-02-05 11:43:43 +1300

    feat: Dev tools page (#763)
    #### Description
    
    A short note covering dev tools at the end of the web guide.

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -164,6 +164,10 @@ export const sidebar = [
 								label: 'Grid',
 								link: 'guides/web/html-css-intro/9-grid/',
 							},
+							{
+								label: 'Dev Tools',
+								link: 'guides/web/html-css-intro/10-devtools/',
+							},
 
 						],
 						collapsed: true,
