43b7208316128823c74f0654eaa1250a18d19b38
EricTey <erictey00@gmail.com>
2026-04-15 21:25:15 +1200

    Ollama Added

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -117,25 +117,27 @@ export const sidebar = [
 						collapsed: true,
 					},
 					{
-						label: 'Projects',
+						label: 'Turtle',
+						link: 'guides/python/turtle/0-setup/',
+					},
+					{
+						label: 'Ollama',
 						items: [
 							{
-								label: 'Turtle',
-								link: 'guides/python/turtle/0-setup/',
+								label: 'Introduction',
+								link: 'guides/python/ollama/0-introduction/',
 							},
 							{
-								label: 'Ollama',
-								items: [
-									{
-										label: 'Introduction',
-										link: 'guides/python/ollama/0-introduction/',
-									},
-									{
-										label: 'API & Advanced Features',
-										link: 'guides/python/ollama/1-api/',
-									},
-								],
-								collapsed: true,
+								label: 'API & Python',
+								link: 'guides/python/ollama/1-api/',
+							},
+							{
+								label: 'Vectors & Embeddings',
+								link: 'guides/python/ollama/2-embeddings/',
+							},
+							{
+								label: 'Modelfiles',
+								link: 'guides/python/ollama/3-modelfiles/',
 							},
 						],
 						collapsed: true,
